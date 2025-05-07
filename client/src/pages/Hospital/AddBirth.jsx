01682054fa449aef8bc13497e035017b426724ba
yacine <yacineddd32@gmail.com>
2025-05-07 22:35:08 +0100

    Rusty lake

--- client/src/pages/Hospital/AddBirth.jsx
+++ client/src/pages/Hospital/AddBirth.jsx
@@ -4,6 +4,7 @@ import { API_PATHS } from "../../utils/apiPaths";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
 
 function AddBirth() {
   useUserAuth();
@@ -41,120 +42,137 @@ function AddBirth() {
   };
 
   return (
-    <div className="w-screen h-screen flex items-center">
-      <div className="form-card mx-auto w-full max-w-4xl">
-        <h2 className="text-2xl font-bold mb-4 text-slate-700">
-          Submit Death Record
-        </h2>
+    <DashboardLayout activeMenu={"Add employees"}>
+      <div className="mt-5">
+        <div className="grid grid-cols-1 md:grid-cols-4 mt-4">
+          <div className="form-card col-span-3">
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl font-medium">Submit Birth Record</h2>
+            </div>
 
-        <form onSubmit={handleSubmit} className="grid gap-4 grid-cols-2">
-          <div className="flex flex-col">
-            <label className="text-xs font-medium text-slate-600">
-              Arabic full name :
-            </label>
+            <div className="grid grid-cols-12 gap-4 mt-2">
+              <div className="col-span-6">
+                <label className="text-xs font-medium text-slate-600">
+                  Arabic full name :
+                </label>
 
-            <input
-              name={"ArabicFullName"}
-              placeholder={"Enter Arabic full name"}
-              className="form-input"
-              value={formData.ArabicFullName}
-              onChange={handleChange}
-            />
+                <input
+                  name={"ArabicFullName"}
+                  placeholder={"Enter Arabic Fullname"}
+                  className="form-input"
+                  value={formData.ArabicFullName}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="col-span-6">
+                <label className="text-xs font-medium text-slate-600">
+                  Latin full name :
+                </label>
+
+                <input
+                  name={"LatinFullName"}
+                  placeholder={"Enter latin full name"}
+                  className="form-input"
+                  value={formData.LatinFullName}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div className="grid grid-cols-12 gap-4 mt-2">
+              <div className="col-span-6">
+                <label className="text-xs font-medium text-slate-600">
+                  Birth Date :
+                </label>
+                <input
+                  type="date"
+                  name="BirthDate"
+                  className="form-input"
+                  value={formData.BirthDate}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+            </div>
+
+            <div className="grid grid-cols-12 gap-4 mt-2">
+              <div className="col-span-6">
+                <label className="text-xs font-medium text-slate-600">
+                  Gender
+                </label>
+
+                <select
+                  name="Gender"
+                  id="Gender"
+                  className="form-input"
+                  value={formData.Gender}
+                  onChange={handleChange}
+                >
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+
+              <div className="col-span-6">
+                <label className="text-xs font-medium text-slate-600">
+                  City :
+                </label>
+
+                <input
+                  name={"City"}
+                  placeholder={"City"}
+                  className="form-input"
+                  value={formData.City}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div className="grid grid-cols-12 gap-4 mt-2">
+              <div className="col-span-6">
+                <label className="text-xs font-medium text-slate-600">
+                  Father's full name :
+                </label>
+
+                <input
+                  name={"FatherName"}
+                  placeholder={"Father's full name"}
+                  className="form-input"
+                  value={formData.FatherName}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="col-span-6">
+                <label className="text-xs font-medium text-slate-600">
+                  Mother's full name :
+                </label>
+
+                <input
+                  name={"MotherName"}
+                  placeholder={"Mother's full name"}
+                  className="form-input"
+                  value={formData.MotherName}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            {message && <p className="mt-4 text-red-600">{message}</p>}
+            <div className="flex justify-end mt-7">
+              <button
+                type="submit"
+                onClick={handleSubmit}
+                className="bg-blue-500 w-full text-white px-4 py-2 col-span-2 rounded hover:bg-blue-700 cursor-pointer"
+              >
+                Submit
+              </button>
+            </div>
           </div>
-          <div className="flex flex-col">
-            <label className="text-xs font-medium text-slate-600">
-            Latin full name :
-            </label>
-
-            <input
-              name={"LatinFullName"}
-              placeholder={"Enter Latin full name"}
-              className="form-input"
-              value={formData.LatinFullName}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col">
-            <label className="text-xs font-medium text-slate-600">
-              Birth Date
-            </label>
-            <input
-              type="date"
-              name="BirthDate"
-              className="form-input"
-              value={formData.BirthDate}
-              onChange={handleChange}
-              required
-            />
-          </div>
-
-          <div className="flex flex-col">
-            <label className="text-xs font-medium text-slate-600">
-            City :
-            </label>
-
-            <input
-              name={"City"}
-              placeholder={"City"}
-              className="form-input"
-              value={formData.City}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="flex flex-col">
-            <label className="text-xs font-medium text-slate-600">Gender</label>
-
-            <select
-              name="Gender"
-              id="Gender"
-              className="form-input"
-              value={formData.Gender}
-              onChange={handleChange}
-            >
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-            </select>
-          </div>
-
-          <div className="flex flex-col">
-            <label className="text-xs font-medium text-slate-600">
-            Father's full name :
-            </label>
-
-            <input
-              name={"FatherName"}
-              placeholder={"Father's full name"}
-              className="form-input"
-              value={formData.FatherName}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="flex flex-col">
-            <label className="text-xs font-medium text-slate-600">
-            Mother's full name :
-            </label>
-
-            <input
-              name={"MotherName"}
-              placeholder={"Mother's full name"}
-              className="form-input"
-              value={formData.MotherName}
-              onChange={handleChange}
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 col-span-2 rounded hover:bg-blue-700"
-          >
-            Submit
-          </button>
-        </form>
-        {message && <p className="mt-4 text-red-600">{message}</p>}
+        </div>
       </div>
-    </div>
+    </DashboardLayout>
   );
 }
 
